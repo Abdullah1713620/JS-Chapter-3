572e5e55cebc3983251ab95fb85f60386e78c5ff
Abdullah1713620 <155411177+Abdullah1713620@users.noreply.github.com>
2024-03-04 06:39:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// Question 1
+
+var age = "I am 22 year old"
+alert(age)
+
+// Question 2
+
+var visit = "You have visited this site 10 times"
+alert(visit)
+
+// Question 3
+var birthYear= 2003
+var message = "This is my birth Year "
+document.write(message , birthYear)
+
+// Question 4
+
+var visitorName = "Abdullah"
+var productTitle = "Tshirt"
+var quantity = 5
+var orderMessage = `${visitorName} ordered ${quantity} ${productTitle}(s) on XYZ clothing store`
+document.write(orderMessage);
